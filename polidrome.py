487576d59ed770d378cd20de9ed71abb8019548a
Andi Alfian Bahtiar <55624363+anddfian@users.noreply.github.com>
2021-08-30 17:10:02 +0800

    first commit
    Membuat File Polindrome.py

---
+++ polidrome.py
@@ -0,0 +1,6 @@
+# Anggota Kelompok:
+# Andi Alfian Bahtiar (2009106002)
+# Alfi Syachdian Noor (2009106023)
+# Muhammad Arsy Dewantara (2009106033)
+# Fathan ghoji adzikra (2009106044)
+# M Amri Rasyid R (2009106047)
