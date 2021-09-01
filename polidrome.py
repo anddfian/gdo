fb78e2b26a5f31219aa8afa2020ef7e4ee3cfc31
Andi Alfian Bahtiar <55624363+anddfian@users.noreply.github.com>
2021-09-01 18:41:53 +0800

    Update polidrome.py

--- polidrome.py
+++ polidrome.py
@@ -1,6 +1,45 @@
-# Anggota Kelompok:
-# Andi Alfian Bahtiar (2009106002)
-# Alfi Syachdian Noor (2009106023)
-# Muhammad Arsy Dewantara (2009106033)
-# Fathan ghoji adzikra (2009106044)
-# M Amri Rasyid R (2009106047)
+import os
+
+def clear_screen():
+    os.system("cls" if os.name == "nt" else "clear")
+
+def palindrome():
+    text = input("Masukkan Teks/Angka : ")
+    print("Hasil Membalikkan   :", text[::-1])
+    print("=====================================")
+    if text == text[::-1]:
+        print("Teks/Angka tersebut Palindrome")
+    else:
+        print("Teks/Angka tersebut Tidak Palindrome")
+    print("=====================================")
+    back_to_menu()
+
+def back_to_menu():
+    input("\nTekan 'Enter' untuk melanjutkan...")
+    show_menu()
+
+def show_menu():
+    clear_screen()
+    print("=====================================")
+    print("|         PROGRAM PALINDROME        |")
+    print("=====================================")
+    print("|       OLEH ANGGOTA KELOMPOK       |")
+    print("=====================================")
+    print("| ANDI ALFIAN BAHTIAR  (2009106002) |")
+    print("| ALFI SYACHDIAN NOOR  (2009106023) |")
+    print("| M. ARSY DEWANTARA    (2009106033) |")
+    print("| FATHAN GHOJI ADZIKRA (2009106044) |")
+    print("| M. AMRI RASYID R.    (2009106047) |")
+    print("=====================================")
+    print("| Info: Tekan CTRL + C untuk keluar |")
+    print("=====================================")
+    try:
+        palindrome()
+    except KeyboardInterrupt:
+        print("\n=====================================")
+        print("|            Terima Kasih           |")
+        print("=====================================")
+        exit()
+
+if __name__ == "__main__":
+    show_menu()
